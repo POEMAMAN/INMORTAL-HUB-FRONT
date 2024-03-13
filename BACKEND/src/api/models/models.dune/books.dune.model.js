43b8d58c922a70c6fc8252e8f72e440af1916377
Alberto Garcia recio <albergarcia89@gmail.com>
2024-03-13 21:23:55 +0100

    fix: change model books

--- BACKEND/src/api/models/models.dune/books.dune.model.js
+++ BACKEND/src/api/models/models.dune/books.dune.model.js
@@ -6,9 +6,7 @@ const booksSchema = new Schema(
   {
     title:{type:String,required:true},
     publication_year: {type: Number},
-    saga:{ type: String, required: false },
     saga_index: {type:Number},
-    universe:{ type: String, required: false },
     author: {type:String, required:true},
     resume: {type:String, required:false},
     picture: {type: String}

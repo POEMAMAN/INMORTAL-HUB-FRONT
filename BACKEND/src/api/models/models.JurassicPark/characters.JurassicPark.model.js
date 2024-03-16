ff26d5f8a10f4b83de4434f1df2eff7301d57255
Jorge hernandez cañas <jorge.hernandez@bootcamp-upgrade.com>
2024-03-16 10:28:20 +0100

    feat: seed alien

--- BACKEND/src/api/models/models.JurassicPark/characters.JurassicPark.model.js
+++ BACKEND/src/api/models/models.JurassicPark/characters.JurassicPark.model.js
@@ -2,14 +2,20 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const charactersSchema = new Schema({
+const charactersSchema = new Schema(
+  {
     name: { type: String, required: true },
     actor: { type: String, required: true },
+    profession: { type: String, required: false },
+    film: { type: String, required: false },
     picture: { type: String },
-    resume: { type: String, required: false },
-}, {
+    resume: {type:String, required:false},
+    
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
 const CharactersJurassicPark = mongoose.model('characterJurassicPark', charactersSchema);
 

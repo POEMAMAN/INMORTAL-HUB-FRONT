898a56a4b4e318166d37fcdb8eb391d9dbae5833
Jorge hernandez cañas <jorge.hernandez@bootcamp-upgrade.com>
2024-03-17 22:49:48 +0100

    feat: seeds StarsWars

--- BACKEND/src/api/models/models.StarWars/movies.StarWars.model.js
+++ BACKEND/src/api/models/models.StarWars/movies.StarWars.model.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const movieSchema = new Schema(
-  {
+const movieSchema = new Schema({
     title: { type: String, required: true },
     picture: { type: String, required: true },
     director: { type: String, required: false },
@@ -12,14 +11,12 @@ const movieSchema = new Schema(
     country: { type: String, required: false },
     genre: { type: String, required: false },
     clasification: { type: String, required: false },
-    books: { type: Schema.ObjectId, ref: 'booksStarWars'},
-    characters: [{ type: Schema.ObjectId, ref: 'characterStarWars'}],
-    resume: {type:String, required:false},
-  },
-  {
+    // books: { type: Schema.ObjectId, ref: 'booksStarWars'},
+    // characters: [{ type: Schema.ObjectId, ref: 'characterStarWars'}],
+    resume: { type: String, required: false },
+}, {
     timestamps: true,
-  }
-);
+});
 
 const MovieStarWars = mongoose.model('movieStarWars', movieSchema);
 module.exports = MovieStarWars;

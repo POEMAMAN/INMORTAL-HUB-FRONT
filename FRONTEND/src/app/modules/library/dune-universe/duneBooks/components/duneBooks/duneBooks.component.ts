e0b249321c5720005ce0462bb0a42330edc3cd34
Firekinger <130169414+dcampos78@users.noreply.github.com>
2024-03-17 09:42:18 +0100

    fix: card books

--- FRONTEND/src/app/modules/library/dune-universe/duneBooks/components/duneBooks/duneBooks.component.ts
+++ FRONTEND/src/app/modules/library/dune-universe/duneBooks/components/duneBooks/duneBooks.component.ts
@@ -1,6 +1,6 @@
 
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component,Input, OnInit,Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-duneBooks',
@@ -8,22 +8,38 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./duneBooks.component.scss']
 })
 export class duneBooksComponent implements OnInit {
-@Input() duneBook: any
+ 
+  @Input() duneBook: any
 isAdmin: boolean = false;
-constructor(private authService: AuthService ){
+  constructor(private renderer: Renderer2, private elementRef: ElementRef, private authService: AuthService) {
+
+  }
+  
+  ngOnInit(): void {
+    this.authService.isAdmin().subscribe({
+      next:(isAdmin: boolean) => {
+        console.log(isAdmin)
+        this.isAdmin = isAdmin;
+      }, 
+      error: (e) => {
+        this.isAdmin = false;
+      }
+    })
+  }
+  
+  flip() {
+    const cardElement = this.elementRef.nativeElement.querySelector('.card');
+    this.renderer.addClass(cardElement, 'flipped');
+  }
+  
+  clickCard() {
+    const clickcardElement = this.elementRef.nativeElement.querySelector('.clickcard');
+    this.renderer.addClass(clickcardElement, 'flipped');
+  }
+  
 
 }
-ngOnInit(): void {
-  this.authService.isAdmin().subscribe({
-    next:(isAdmin: boolean) => {
-      console.log(isAdmin)
-      this.isAdmin = isAdmin;
-    }, 
-    error: (e) => {
-      this.isAdmin = false;
-    }
-  })
-}
-}
-  
+
+
+
 

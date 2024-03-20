aec1f0bd45032a0aac56659a2ab2733f1d9d3c14
Firekinger <130169414+dcampos78@users.noreply.github.com>
2024-03-20 02:22:59 +0100

    feat: avatar header

---
+++ FRONTEND/src/app/core/avatar/avatar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.css']
+})
+export class AvatarComponent {
+  avatarUrl: string ="";
+;
+onFileSelected(event: Event): void {
+  const inputElement = event.target as HTMLInputElement;
+
+  if (inputElement && inputElement.files && inputElement.files.length > 0) {
+    const file: File = inputElement.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      if (e.target && e.target.result) {
+        this.avatarUrl = e.target.result as string;
+      }
+    };
+
+    reader.readAsDataURL(file);
+  }
+}
+}

b2de5a2ec33cee5be3d0b11aa1c3e4d28bcde51b
poemaman <luis.campos.pousa@gmail.com>
2024-03-25 10:25:19 +0100

    fix: images library and main

--- FRONTEND/src/app/modules/library/terminator-universe/terminatorCharacters/interfaces/TerminatorCharacters.interface.ts
+++ FRONTEND/src/app/modules/library/terminator-universe/terminatorCharacters/interfaces/TerminatorCharacters.interface.ts
@@ -1,12 +1,10 @@
 export interface TerminatorCharacter {
   name: String,
+  actor: String,
+  race: String,
   profession: String,
-  rank: String,
-  birthday: String,
-  planet: String,
-  country: String,
-  resume: String,
   picture: String,
+  resume: String,
 
 }
 

3747bf83f1ad4d33a6e3016e47eefc5b02d472bf
POEMAMAN <130597285+POEMAMAN@users.noreply.github.com>
2024-04-17 18:22:47 +0200

    Delete BACKEND directory

--- BACKEND/src/api/chat/chat.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const chatsController = require('./chat.controller');
-
-const router = express.Router();
-
-router.get('/chats', chatsController.getChats);
-
-module.exports = router;
--- BACKEND/src/utils/error/error.js
+++
@@ -1,9 +0,0 @@
-// Función para formatear errores y no repetir estas líneas constantemente
-const setError = (code, message) => {
-    const error = new Error;
-    error.code = code;
-    error.message = message;
-    return error
-}
-
-module.exports = { setError };
